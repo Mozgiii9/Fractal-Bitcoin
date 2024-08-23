bb8e17e2aa9b4c07d5a40ba2c94e5c291668c4cd
Nikita Mozgin <74683169+Mozgiii9@users.noreply.github.com>
2024-08-23 16:39:19 +0300

    Update fractalremove.sh

--- fractalremove.sh
+++ fractalremove.sh
@@ -14,7 +14,7 @@ sudo systemctl daemon-reload
 
 # Удаление файлов программы и данных
 status_message "Удаление файлов программы и данных..."
-rm -rf /root/fractald-1.0.6-x86_64-linux-gnu
+rm -rf /root/fractald-1.0.7-x86_64-linux-gnu
 rm -f /root/.bitcoin/wallets/wallet/wallet.dat
 rm -f /root/.bitcoin/wallets/wallet/MyPK.dat
 
@@ -22,10 +22,5 @@ rm -f /root/.bitcoin/wallets/wallet/MyPK.dat
 status_message "Очистка системных журналов..."
 sudo journalctl --rotate
 sudo journalctl --vacuum-time=1s
-
-# Удаление установленных зависимостей (если больше не нужны)
-status_message "Удаление установленных зависимостей..."
-sudo apt-get remove --purge curl build-essential pkg-config libssl-dev git wget jq make gcc chrony -y
-sudo apt-get autoremove -y
 
 status_message "Удаление ноды Fractal Bitcoin завершено!"
